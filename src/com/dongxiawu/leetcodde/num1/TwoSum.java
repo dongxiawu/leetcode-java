5be1cdc1f43466f5720ae9c890d471099f9ced4d
dongxia.wdx <dongxia.wdx@alibaba-inc.com>
2020-01-20 00:03:30 +0800

    fix 完善 two sum 方法

--- src/com/dongxiawu/leetcodde/num1/TwoSum.java
+++ src/com/dongxiawu/leetcodde/num1/TwoSum.java
@@ -1,15 +1,23 @@
 package com.dongxiawu.leetcodde.num1;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  *
  */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        return twoSumSolution3(nums, target);
+    }
+
+    private int[] twoSumSolution1(int[] nums, int target) {
+        // 复制，保持原有顺序
         int[] newNums = Arrays.copyOf(nums, nums.length);
+        // 排序
         Arrays.sort(newNums);
         int[] result = new int[2];
+        // 找目标数
         for (int i = 0, j = newNums.length -1; i < j;) {
             int total = newNums[i] + newNums[j];
             if (total == target) {
@@ -22,6 +30,7 @@ public class TwoSum {
                 i++;
             }
         }
+        // 找目标位置
         int[] pos = new int[]{-1, -1};
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == result[0] && pos[0] == -1) {
@@ -31,5 +40,36 @@ public class TwoSum {
             }
         }
         return pos;
+    }
+
+    private int[] twoSumSolution2(int[] nums, int target) {
+        // 保存值和顺序的map
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        // 找答案
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                // 获取顺序
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    private int[] twoSumSolution3(int[] nums, int target) {
+        // 边找顺序边保存
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                // 获取顺序
+                return new int[]{i, map.get(complement)};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
